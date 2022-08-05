bab3af8ad506508e05a8a3856c0f2c56e001e917
Jack M <jackm550@googlemail.com>
2022-08-05 11:40:43 +0100

    Added Domain Models as a draft and a basic controller

---
+++ HotelsBooking.Api/Controllers/HotelsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelsBooking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelsController : Controller
+    {
+        public HotelsController()
+        { }
+
+        [HttpGet]
+        public IActionResult GetRooms()
+        {
+            return Ok("Hello From The Hotels Controller!");
+        }
+    }
+}
