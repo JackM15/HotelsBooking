febbe1a642eaa0adf68ef84f6ed64afbfa4eff38
Jack M <jackm550@googlemail.com>
2022-08-18 14:26:02 +0100

    Added data annotations to the Create DTO

--- HotelsBooking.Api/Dtos/HotelCreateDto.cs
+++ HotelsBooking.Api/Dtos/HotelCreateDto.cs
@@ -1,14 +1,22 @@
 ﻿using HotelsBooking.Domain.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelsBooking.Api.Dtos
 {
     public class HotelCreateDto
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
+        [Range(1,5)]
         public int Stars { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Country { get; set; }
         public string Description { get; set; }
     }
